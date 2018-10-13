f1392502bde7cb813606ea9b65c9ae51c05d51df
Dave Dolben <davedolben@improbable.io>
2018-10-12 17:36:49 -0700

    WIP actor pointer array test

--- SpatialGDK/Source/Private/EngineClasses/SpatialActorChannel.cpp
+++ SpatialGDK/Source/Private/EngineClasses/SpatialActorChannel.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+#pragma optimize("", off)
 
 #include "SpatialActorChannel.h"
 
@@ -699,3 +700,4 @@ void USpatialActorChannel::SpatialViewTick()
 		}
 	}
 }
+#pragma optimize("", on)
